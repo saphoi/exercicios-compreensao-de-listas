9d438b7eacb726e8d8de655a193ec298e8a8b28e
Sophia <eloiselamenha@gmail.com>
2024-05-16 11:37:02 -0300

    Update ex2.hs

--- ex2.hs
+++ ex2.hs
@@ -1,7 +1,2 @@
-import Data.Set (fromList, toList)
-
-organizar :: (Eq a, Ord a) => [a] -> [a]
-organizar = toList . fromList
-
 grid :: Int -> Int -> [(Int,Int)]
-grid x y = organizar [(x,y) | y <- [0,x,y], x <- [0,x]]
+grid x y = [(x,y) | y <- [0..y], x <- [0..x]]
